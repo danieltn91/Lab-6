f81240abcf3987eafd6cff1c62533b29733a2033
Daniel Nguyen <danieltn91@gmail.com>
2015-12-01 20:32:40 -0800

    Update Intellect.java

--- src/Intellect.java
+++ src/Intellect.java
@@ -2,9 +2,13 @@
 //	Intellect needs to be rated.
 //	Intellect must be considered intellectual or not
 public class Intellect {
+	private Evidence evidence;
 	private int IQ;
 	private boolean isIntellectual;
-	
+	public Evidence generate()
+	{
+		return evidence;
+	}
 	public int getIQ() {
 		return IQ;
 	}
