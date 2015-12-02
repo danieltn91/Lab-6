29011ea54017f0042ea2a435cef416be0c8f84bb
Daniel Nguyen <danieltn91@gmail.com>
2015-12-01 20:58:41 -0800

    Update Event.java

--- src/Event.java
+++ src/Event.java
@@ -1,5 +1,6 @@
-
+import java.util.List;
 public class Event {
+	private List<Media> medias;
 	private String eventName;
 	private String eventLocation;
 	private ArrayList<String> attendees;
@@ -21,4 +22,18 @@ public class Event {
 	public ArrayList<String> getAttendees() {
 		return attendees;
 	}
+	public void recordedOn(List<Media> anyMedias) 
+	{   
+		this.medias = anyMedias;  
+	}
+	public List<Media> getMedias()
+	{   
+		return medias;
+	}
+	public void setMedias(List<Media> medias) 
+	{  
+		this.medias = medias;  
+		
+	}
+	
 }
